8fd48dc6b0fe61047f735ecd2b69bb0d54f1fa2e
Ivan Lorenzo <elhibar77@hotmail.es>
2025-07-09 10:14:55 +0200

    Fix: creado el archivo Routes.jsx y traslado del código de las rutas

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,17 @@
+import {Routes, Route} from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import About from './components/Footer/pages/About';
+import Privacy from './components/Footer/pages/Privacy';
+import Terms from './components/Footer/pages/Terms';
+import WorkWithUs from './components/Footer/pages/WorkWIthUs';
+
+export default function AppRoutes() {
+    return (
+    <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/work-with-us" element={<WorkWithUs />} />
+      </Routes>
+    );
+}
