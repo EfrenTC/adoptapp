160c333f382082450298b9f454c6ffa667ef5b58
vanderleneo <vanderleneo>
2025-07-10 10:30:23 +0200

    OPS-13 feet: estrutura y stilos de las cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 
-import './App.css'
-import Api from './services/Api'
-import AppRoutes from "./routes/Routes";
+import React from 'react';
+import './App.css'; 
+import AppRoutes from './routes/Routes';
 import Footer from './components/Footer/Footer';
 
-
 function App() {
+ 
+ 
 
   return (
     <>
-    <AppRoutes>
-
-    </AppRoutes>
-
+     
+      <AppRoutes />
+      <Footer />
     </>
   );
 }
 
 export default App;
+
