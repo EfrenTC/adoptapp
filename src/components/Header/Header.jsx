b90b7359b9d2a3d1ca8dbf35dd4c90a14d75a9a4
Ivan Lorenzo <elhibar77@hotmail.es>
2025-07-15 19:11:03 +0200

    OPS-28 feat: añadido botón y funcionalidad para el modo claro/oscuro y las primeras variables css necesarias para cambiarlo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,60 +1,64 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "./Header.css";
-import { useState } from "react";
 import logo from "../../assets/logo.png";
 import { Link } from 'react-router-dom';
-
+import { ThemeContext } from '../ThemeContext/ThemeContext.jsx';
+import Toggle from '../ThemeContext/ThemeToggle.jsx';
 
 const Header = () => {
-    const [menuAbierto, setMenuAbierto] = useState(false);
+  const [menuAbierto, setMenuAbierto] = useState(false);
+  useContext(ThemeContext);
 
-    const toggleMenu = () => setMenuAbierto(!menuAbierto);
+  const toggleMenu = () => setMenuAbierto(!menuAbierto);
 
-    return (
-        <div className="header">
-            <div className="header__logo">
-                <img src={logo} alt="Logo Adoptapp" className="header__logo-img" />
-                <span className="header__brand"></span>
-            </div>
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src={logo} alt="Logo Adoptapp" className="header__logo-img" />
+        <span className="header__brand"></span>
+      </div>
 
-            <button className="header__burger"
-                onClick={toggleMenu}
-                aria-label="Menú">
-                <span className="header__burger-line"></span>
-                <span className="header__burger-line"></span>
-                <span className="header__burger-line"></span>
-            </button>
+      <button
+        className="header__burger"
+        onClick={toggleMenu}
+        aria-label="Menú"
+      >
+        <span className="header__burger-line"></span>
+        <span className="header__burger-line"></span>
+        <span className="header__burger-line"></span>
+      </button>
 
-          <nav className={`header__nav ${menuAbierto ? "header__nav--open" : ""}`}>
-  <Link to="/" className="header__nav-link">Inicio</Link>
-  <Link to="/adopcion" className="header__nav-link">Adopta</Link>
-  <Link to="/consejos" className="header__nav-link">Consejos</Link>
-  <Link to="/historias" className="header__nav-link">Historias</Link>
-  
-  <Link   to="/favoritos"
-  className="header__nav-link header__nav-link--icon"
-  title="Ver favoritos"
-  aria-label="Ver favoritos">
-    Favoritos
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="#F87171"
-      className="header__icon-heart"
-    >
-      <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
-    </svg>
-  </Link>
+      <nav className={`header__nav ${menuAbierto ? "header__nav--open" : ""}`}>
+        <Link to="/" className="header__nav-link">Inicio</Link>
+        <Link to="/adopcion" className="header__nav-link">Adopta</Link>
+        <Link to="/consejos" className="header__nav-link">Consejos</Link>
+        <Link to="/historias" className="header__nav-link">Historias</Link>
 
-  <Link to="/contacto" className="header__nav-link">Contacto</Link>
-  <Link to="/donar" className="header__donar-btn">DONAR</Link>
-</nav>
+        <Link
+          to="/favoritos"
+          className="header__nav-link header__nav-link--icon"
+          title="Ver favoritos"
+          aria-label="Ver favoritos"
+        >
+          Favoritos
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="#F87171"
+            className="header__icon-heart"
+          >
+            <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+          </svg>
+        </Link>
 
+        <Link to="/contacto" className="header__nav-link">Contacto</Link>
+        <Link to="/donar" className="header__donar-btn">DONAR</Link>
 
-
-
-        </div>
-    );
+ 
+        <Toggle></Toggle>
+      </nav>
+    </div>
+  );
 };
 
-export default Header; 
+export default Header;
